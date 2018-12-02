30fceaccc2750f4eee4a45bb03ea3cb7ae8b9465
Chelsi Jones <cajones.bz@gmail.com>
2018-12-02 14:39:50 -0600

    Add date-fns

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import './styles/main.scss'
+import './styles/main.scss';
+import { format } from 'date-fns';
+
+const showDate = () => {
+    return format(new Date(2014, 20, 2), 'MMMM Do, YYYY');
+}
+
+console.log(showDate());
